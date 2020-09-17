cdb81253664a494812379b9b173df55d1cc93b81
Hugo Holm <hugo.tiger.holm@gmail.com>
2020-09-17 13:17:13 +0200

    Added willHandover-state to keep track of which storys to fade out

--- src/components/story-card/index.js
+++ src/components/story-card/index.js
@@ -12,10 +12,10 @@ const transition = { duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96] };
 export function StoryCard({ size = "regular", story, imageSrc, ...restProps }) {
   const { setBox } = useTransition();
   const history = useHistory();
-  // const [willHandover, setWillHandover] = useState(false);
+  const [willHandover, setWillHandover] = useState(false);
 
   const handleClick = (event) => {
-    // setWillHandover(true);
+    setWillHandover(true);
     setBox(event.currentTarget);
     history.push(`/story/${story.id}`);
   };
@@ -26,12 +26,13 @@ export function StoryCard({ size = "regular", story, imageSrc, ...restProps }) {
     <Styled.StoryCard {...restProps}>
       <FancyImg
         aspectRatio={size === "regular" && 4 / 6}
-        whileHover={{ scale: 1.03 }}
+        whileHover={{ scale: willHandover ? 1 : 1.03 }}
         whileTap={{ scale: 1 }}
         src={image}
         style={{ cursor: "pointer" }}
         onClick={handleClick}
         role="button"
+        exit={{ opacity: willHandover ? 1 : 0 }}
       />
       <motion.h5 exit={{ opacity: 0, transition: { ...transition } }}>
         {story.location} <span className="country">{story.country}</span>
