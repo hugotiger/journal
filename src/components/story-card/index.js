768d0694256d58840538d06a785dc123a0d15dd4
Hugo Holm <hugo.tiger.holm@gmail.com>
2020-09-18 16:58:42 +0200

    Now uses handoverId to determine exit-animation

--- src/components/story-card/index.js
+++ src/components/story-card/index.js
@@ -7,15 +7,16 @@ import { motion } from "framer-motion";
 // Hooks
 import { useTransition } from "../../hooks";
 
-const transition = { duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96] };
+const transition = { duration: 0.3, ease: [0.43, 0.13, 0.23, 0.96] };
 
 export function StoryCard({ size = "regular", story, imageSrc, ...restProps }) {
-  const { setBox } = useTransition();
+  const { setBox, handoverId, setHandoverId } = useTransition();
   const history = useHistory();
   const [willHandover, setWillHandover] = useState(false);
 
   const handleClick = (event) => {
     setWillHandover(true);
+    setHandoverId(story.id);
     setBox(event.currentTarget);
     history.push(`/story/${story.id}`);
   };
@@ -26,13 +27,24 @@ export function StoryCard({ size = "regular", story, imageSrc, ...restProps }) {
     <Styled.StoryCard {...restProps}>
       <FancyImg
         aspectRatio={size === "regular" && 4 / 6}
-        whileHover={{ scale: willHandover ? 1 : 1.03 }}
+        whileHover={{
+          scale: willHandover ? 1 : 1.03,
+          transition: {
+            ...transition,
+            duration: 0.6,
+          },
+        }}
         whileTap={{ scale: 1 }}
         src={image}
         style={{ cursor: "pointer" }}
         onClick={handleClick}
         role="button"
-        exit={{ opacity: willHandover ? 1 : 0 }}
+        initial={handoverId === story.id ? {} : { opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{
+          opacity: willHandover ? 1 : 0,
+        }}
+        transition={transition}
       />
       <motion.h5 exit={{ opacity: 0, transition: { ...transition } }}>
         {story.location} <span className="country">{story.country}</span>
