e0f72411a109f3dcf4718c34a4e22883ef553e6c
Hugo Holm <hugo.tiger.holm@gmail.com>
2020-09-21 13:46:32 +0200

    Added exports file

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Alert } from "./alert";
+export { default as BackButton } from "./back-button";
+export { default as FancyImg } from "./fancy-image";
+export { default as Header } from "./header";
+export { default as Section } from "./section";
+export { default as StoryCard } from "./story-card";
